69099868a0ead97067ae177a6eb9ba855b51558e
Kieron Palmer <Kieron@kier-solutions.co.uk>
2025-10-24 17:02:13 +0100

    AI'd version

--- Get-CalendarPermissions.ps1
+++ Get-CalendarPermissions.ps1
@@ -1,19 +1,25 @@
-#$Mailboxes=Get-Mailbox | Where-Object{$_.isshared -eq $true}
-$Mailboxes=Get-Mailbox -RecipientTypeDetails SharedMailbox
+$Mailboxes = Get-Mailbox -RecipientTypeDetails SharedMailbox
+$Results = @()
+
+foreach ($mbx in $Mailboxes) {
+    $smtp = $mbx.PrimarySmtpAddress
+    $path = "$smtp:\Calendar"
+
+    try {
+        $perms = Get-MailboxFolderPermission -Identity $path -ErrorAction Stop
+        foreach ($perm in $perms) {
+            $Results += [pscustomobject]@{
+                Mailbox      = $smtp
+                Folder       = $perm.FolderName
+                User         = $perm.User
+                AccessRights = $perm.AccessRights -join ';'
+            }
+        }
+    }
+    catch {
+        Write-Warning "Failed to get permissions for $smtp ($path): $_"
+    }
+}
 
 $OutPath = ".\Output.csv"
-
-foreach($mbx in $Mailboxes){
-    $smtp=$mbx.primarysmtpaddress
-    $path=$smtp + ":\Calendar" 
-    $perms=Get-MailboxFolderPermission -Identity $path
-        foreach($perm in $perms){
-            if($perm.user -notmatch "Default" -and $perm.user -notmatch "anonymous"){
-             $OutputVar=    $smtp + "," + $perm.foldername + "," + $perm.user + "," + $perm.accessrights
-             
-             Out-File -FilePath $OutPath -InputObject $OutputVar -Append
-            }
-
-        }
-       
-}
+$Results | Export-Csv -Path $OutPath -NoTypeInformation -Encoding UTF8
